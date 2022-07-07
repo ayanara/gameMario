76963182c0b4537f4cbe05bb4fa05efeb0db828a
Ayanara <aya.nathane@gmail.com>
2022-07-06 23:24:02 -0300

    finalizar função JUMP no game

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -9,3 +10,17 @@ const jump = () => {
     }, 500);
 }
 document.addEventListener('keydown', jump); 
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    
+
+    
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    }
+
+}, 10);
